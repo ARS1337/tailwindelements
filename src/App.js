88ca41094027d4a3dc45ecf8f44e61e67a79e2e3
abhaySingh <abhaysingh17498@gmail.com>
2022-08-03 10:58:35 +0530

    working tailwind-elements example

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Accordion from "./components/Accordion";
+
+function App() {
+  return (
+    // <div className="w-full flex items-center justify-center h-[100vh]">
+      <Accordion/>
+    // </div>
+  );
+}
+
+export default App;
